5051b13eb5a478660a802bef512aa50bcb39425c
Leonardo Fernandes <leonardo.gomes@lit.ifce.edu.br>
2021-09-14 09:21:33 -0300

    Upping new UIFont and setup main viewController

--- Revisadinho/MaintanenceScene/Views/ViewControllers/ViewController.swift
+++ Revisadinho/MaintanenceScene/Views/ViewControllers/ViewController.swift
@@ -12,17 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let thirdHeightView = self.view.frame.height / 3
-        let redSquare = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: thirdHeightView))
-        redSquare.backgroundColor = .red
-        let greenSquare = UIView(frame: CGRect(x: 0, y: thirdHeightView, width: self.view.frame.width, height: thirdHeightView))
-        greenSquare.backgroundColor = .green
-  
-        let blueSquare = UIView(frame: CGRect(x: 0, y: thirdHeightView + thirdHeightView, width: self.view.frame.width, height: thirdHeightView))
-        blueSquare.backgroundColor = .blue
-        view.addSubview(redSquare)
-        view.addSubview(greenSquare)
-        view.addSubview(blueSquare)
     }
 
 }
