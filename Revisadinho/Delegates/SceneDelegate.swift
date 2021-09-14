96628938b363a43b05dfda94059999624b42e141
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2021-09-14 11:56:13 -0300

    Changing the root viewController

--- Revisadinho/Delegates/SceneDelegate.swift
+++ Revisadinho/Delegates/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
                 
-        let viewController = ViewController()
+        let viewController = MaintenanceViewController()
         
         window.rootViewController = viewController
 
