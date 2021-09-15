e722dd5c02ff242cbb0a09e1d5be9bfe7bdf7c3c
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2021-09-15 11:23:31 -0300

    hiding navigation bar

--- Revisadinho/MaintanenceScene/Views/ViewControllers/ViewController.swift
+++ Revisadinho/MaintanenceScene/Views/ViewControllers/ViewController.swift
@@ -11,6 +11,7 @@ class MaintenanceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
         // Do any additional setup after loading the view.
     }
     
