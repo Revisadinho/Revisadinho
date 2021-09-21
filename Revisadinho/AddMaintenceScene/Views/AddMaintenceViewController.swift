be2b28fe767002c332ee5fefed420282b2eef3e1
jessicaguiot <jessica.guiot97@gmail.com>
2021-09-21 12:08:39 -0300

    integrating with view model

--- Revisadinho/AddMaintenceScene/Views/AddMaintenceViewController.swift
+++ Revisadinho/AddMaintenceScene/Views/AddMaintenceViewController.swift
@@ -17,6 +17,8 @@ class AddMaintenceViewController: UIViewController {
         return viewController
     }()
     
+    lazy var viewModel = AddMaintenanceViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setCalendarTextFieldDelegate()
@@ -24,7 +26,7 @@ class AddMaintenceViewController: UIViewController {
     
     override func loadView() {
         view = contentView
-        contentView.saveButton.addTarget(self, action: #selector(printInputs), for: .touchUpInside)
+        contentView.saveButton.addTarget(self, action: #selector(saveInputs), for: .touchUpInside)
     }
     
     private func showBottomSheetView() {
@@ -33,14 +35,18 @@ class AddMaintenceViewController: UIViewController {
     }
     
     @objc
-    func printInputs() {
-        guard let totalKm = contentView.hodometerTextField.text else {return}
+    func saveInputs() {
+        guard let hodometerText = contentView.hodometerTextField.text else {return}
         guard let date = bottomSheetViewController.selectedDate else {return}
         let services = contentView.servicesSelected
         
-        print(totalKm)
-        print(bottomSheetViewController.getDateString(date))
-        print(services)
+        if let totalKm = Double(hodometerText) {
+            if viewModel.saveMaintenance(hodometer: totalKm, date: date, maintenanceItens: services) {
+                dismiss(animated: true, completion: nil)
+            } else {
+                print("ERROR")
+            }
+        }
     }
 }
 
